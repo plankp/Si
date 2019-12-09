859058321f6155f5ae45e5f22622c3ddad92082c
Paul Teng <plankp@outlook.com>
2019-12-09 18:21:49 -0500

    Add back a test that disappeared
    SERIOUSLY! WHY DOES THIS KEEP HAPPENING!?

--- src/test/java/com/ymcmp/midform/tac/EmulatorTest.java
+++ src/test/java/com/ymcmp/midform/tac/EmulatorTest.java
@@ -206,4 +206,94 @@ public class EmulatorTest {
         subCaller.optimize();
         Assert.assertEquals(Tuple.from(new ImmCharacter('B'), new ImmCharacter('A')), this.emulator.callSubroutine(subCaller));
     }
+
+    @Test
+    public void testSynthEvenOdd() {
+        // function is_odd(n) {
+        // _entry:
+        //   eq.ii %b0, %b1, n, 0
+        // %b0:
+        //   ret false
+        // %b1:
+        //   sub.ii %t0, n, 1
+        //   tailcall is_even %t0
+        // }
+        //
+        // function is_even(n) {
+        // _entry:
+        //   eq.ii %b0, %b1, n, 0
+        // %b0:
+        //   ret true
+        // %b1:
+        //   sub.ii %t0, n, 1
+        //   tailcall is_odd %t0
+        // }
+
+        final Subroutine subIsOdd = new Subroutine("is_odd", new FunctionType(ImmInteger.TYPE, ImmBoolean.TYPE));
+        final Subroutine subIsEven = new Subroutine("is_even", new FunctionType(ImmInteger.TYPE, ImmBoolean.TYPE));
+
+        {
+            // is_odd function
+            final Binding.Immutable n = new Binding.Immutable("n", ImmInteger.TYPE);
+            subIsOdd.setParameters(Collections.singletonList(n));
+
+            final Block entry = new Block("_entry");
+            final Block b0 = new Block("%b0");
+            final Block b1 = new Block("%b1");
+
+
+            final Binding.Immutable t0 = new Binding.Immutable("%t0", ImmInteger.TYPE);
+
+            entry.setStatements(Collections.singletonList(
+                    new ConditionalJumpStatement(ConditionalJumpStatement.ConditionalOperator.EQ_II, b0, b1, n, new ImmInteger(0))));
+
+            b0.setStatements(Collections.singletonList(
+                    new ReturnStatement(new ImmBoolean(false))));
+
+            b1.setStatements(Arrays.asList(
+                    new BinaryStatement(BinaryStatement.BinaryOperator.SUB_II, t0, n, new ImmInteger(1)),
+                    new TailCallStatement(new FuncRef.Local(subIsEven), t0)));
+
+            subIsOdd.setInitialBlock(entry);
+        }
+
+        {
+            // is_even function
+            final Binding.Immutable n = new Binding.Immutable("n", ImmInteger.TYPE);
+            subIsEven.setParameters(Collections.singletonList(n));
+
+            final Block entry = new Block("_entry");
+            final Block b0 = new Block("%b0");
+            final Block b1 = new Block("%b1");
+
+
+            final Binding.Immutable t0 = new Binding.Immutable("%t0", ImmInteger.TYPE);
+
+            entry.setStatements(Collections.singletonList(
+                    new ConditionalJumpStatement(ConditionalJumpStatement.ConditionalOperator.EQ_II, b0, b1, n, new ImmInteger(0))));
+
+            b0.setStatements(Collections.singletonList(
+                    new ReturnStatement(new ImmBoolean(true))));
+
+            b1.setStatements(Arrays.asList(
+                    new BinaryStatement(BinaryStatement.BinaryOperator.SUB_II, t0, n, new ImmInteger(1)),
+                    new TailCallStatement(new FuncRef.Local(subIsOdd), t0)));
+
+            subIsEven.setInitialBlock(entry);
+        }
+
+        subIsOdd.validate();
+        subIsEven.validate();
+        Assert.assertEquals(new ImmBoolean(false), this.emulator.callSubroutine(subIsOdd, new ImmInteger(6)));
+        Assert.assertEquals(new ImmBoolean(true), this.emulator.callSubroutine(subIsEven, new ImmInteger(6)));
+        Assert.assertEquals(new ImmBoolean(true), this.emulator.callSubroutine(subIsOdd, new ImmInteger(5)));
+        Assert.assertEquals(new ImmBoolean(false), this.emulator.callSubroutine(subIsEven, new ImmInteger(5)));
+
+        subIsOdd.optimize();
+        subIsEven.optimize();
+        Assert.assertEquals(new ImmBoolean(false), this.emulator.callSubroutine(subIsOdd, new ImmInteger(6)));
+        Assert.assertEquals(new ImmBoolean(true), this.emulator.callSubroutine(subIsEven, new ImmInteger(6)));
+        Assert.assertEquals(new ImmBoolean(true), this.emulator.callSubroutine(subIsOdd, new ImmInteger(5)));
+        Assert.assertEquals(new ImmBoolean(false), this.emulator.callSubroutine(subIsEven, new ImmInteger(5)));
+    }
 }
