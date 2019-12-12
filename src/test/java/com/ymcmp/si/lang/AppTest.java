86e7815afd9384e311171b04ee7efa3ce6a239cd
Paul Teng <plankp@outlook.com>
2019-12-11 21:13:28 -0500

    Test App

---
+++ src/test/java/com/ymcmp/si/lang/AppTest.java
@@ -0,0 +1,53 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+package com.ymcmp.si.lang;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test(expected = CompileTimeException.class)
+    public void testInexistentInputFile() {
+        final String file = "www.xxx.yyy.zzz";
+        Assert.assertFalse(new File(file).exists());
+        App.main(new String[]{ file });
+    }
+
+    @Test
+    public void testEmitTACOnAllSpecFiles() {
+        final File path = new File("./spec/");
+        final File[] proclist = path.listFiles((file, name) -> name.endsWith(".si"));
+
+        final File outdir = new File("./spec/tacgen/");
+        outdir.mkdir();
+
+        // generate the files then delete them
+        for (final File file : proclist) {
+            final String name = file.getAbsolutePath();
+            final String output = outdir.getAbsolutePath() + '/' + file.getName() + ".tac";
+
+            App.main(new String[] { "-i", "-t", "-o", output, name });
+        }
+    }
+
+    @Test
+    public void testEmitC99OnAllSpecFiles() {
+        final File path = new File("./spec/");
+        final File[] proclist = path.listFiles((file, name) -> name.endsWith(".si"));
+
+        final File outdir = new File("./spec/c99gen/");
+        outdir.mkdir();
+
+        // generate the files then delete them
+        for (final File file : proclist) {
+            final String name = file.getAbsolutePath();
+            final String output = outdir.getAbsolutePath() + '/' + file.getName() + ".c";
+
+            App.main(new String[] { "-c", "-t", "-o", output, name });
+        }
+    }
+}
