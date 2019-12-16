8540ac83304f5204b0a7435e8e07f3811f699e17
Paul Teng <plankp@outlook.com>
2019-12-16 08:50:08 -0500

    Redo how declarations are handled

---
+++ src/main/java/com/ymcmp/si/lang/Pair.java
@@ -0,0 +1,15 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+package com.ymcmp.si.lang;
+
+public final class Pair<A, B> {
+
+    public final A a;
+    public final B b;
+
+    public Pair(A a, B b) {
+        this.a = a;
+        this.b = b;
+    }
+}
