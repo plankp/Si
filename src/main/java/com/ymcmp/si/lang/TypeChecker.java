082ac6fd6eea2742bc9b8d4e2729344425aabf89
Paul Teng <plankp@outlook.com>
2019-12-16 11:14:36 -0500

    Type declarations complete

--- src/main/java/com/ymcmp/si/lang/TypeChecker.java
+++ src/main/java/com/ymcmp/si/lang/TypeChecker.java
@@ -8,6 +8,7 @@ import static com.ymcmp.si.lang.LiteralUtils.*;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -31,6 +32,8 @@ public final class TypeChecker extends SiBaseVisitor<Object> {
 
     private final Map<Path, SiParser.FileContext> importMap = new LinkedHashMap<>();
 
+    private final Scope<String, TypeBank<Type, Boolean>> definedTypes = new Scope<>();
+
     private final LinkedList<InstantiatedFunction.Local> ifuncQueue = new LinkedList<>();
 
     private String namespacePrefix;
@@ -43,6 +46,8 @@ public final class TypeChecker extends SiBaseVisitor<Object> {
 
     public void reset() {
         this.importMap.clear();
+
+        this.definedTypes.clear();
 
         this.ifuncQueue.clear();
 
@@ -92,6 +97,9 @@ public final class TypeChecker extends SiBaseVisitor<Object> {
 
         // sort the queued declarations by their process priority
         queue.sort((a, b) -> Integer.compare(getProcessPriority(a.b), getProcessPriority(b.b)));
+
+        // setup global variables
+        this.definedTypes.enter();
 
         // then process the declarations
         Pair<String, SiParser.TopLevelDeclContext> item;
@@ -180,22 +188,211 @@ public final class TypeChecker extends SiBaseVisitor<Object> {
 
     @Override
     public Object visitDeclTypeAlias(SiParser.DeclTypeAliasContext ctx) {
-        System.out.println("scope =   " + this.namespacePrefix);
-        System.out.println("  type:   " + ctx.name.getText());
+        final String name = this.namespacePrefix + '\\' + ctx.name.getText();
+        final TypeBank<Type, Boolean> bank = definedTypes.getCurrentOrInit(name, TypeBank::new);
+
+        final Type aliased;
+        if (ctx.generic != null) {
+            this.definedTypes.enter();
+            final List<FreeType> boundary = this.visitDeclGeneric(ctx.generic);
+            final ParametricType<Type> pt = new ParametricType<>(this.visitCoreTypes(ctx.type), boundary);
+            aliased = pt;
+            bank.addParametricType(pt, this.isExported);
+            this.definedTypes.exit();
+        } else {
+            final Type t = this.visitCoreTypes(ctx.type); 
+            aliased = t;
+            bank.setSimpleType(t, this.isExported);
+        }
+
+        System.out.println("scope  =   " + this.namespacePrefix);
+        System.out.println("alias  :   " + ctx.name.getText());
+        System.out.println(" type  :   " + aliased);
         return null;
     }
 
     @Override
+    public FreeType visitParamFreeType(SiParser.ParamFreeTypeContext ctx) {
+        return new FreeType(ctx.name.getText());
+    }
+
+    @Override
+    public FreeType visitParamEquivType(SiParser.ParamEquivTypeContext ctx) {
+        return new FreeType(ctx.name.getText(), (Type) this.visit(ctx.bound));
+    }
+
+    @Override
+    public List<FreeType> visitDeclGeneric(SiParser.DeclGenericContext ctx) {
+        final ArrayList<FreeType> list = new ArrayList<>(ctx.args.size());
+        for (final SiParser.GenericParamContext t : ctx.args) {
+            final FreeType bound = (FreeType) this.visit(t);
+            list.add(bound);
+
+            // need to add it to the current type scope
+            final String name = bound.getName();
+            final TypeBank<Type, Boolean> bank = this.definedTypes.getCurrentOrInit(name, TypeBank::new);
+
+            try {
+                bank.setSimpleType(bound.expandBound(), true);
+            } catch (DuplicateDefinitionException ex) {
+                throw new DuplicateDefinitionException("Duplicate type parameter: " + name, ex);
+            }
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<Type> visitTypeParams(SiParser.TypeParamsContext ctx) {
+        final ArrayList<Type> list = new ArrayList<>(ctx.types.size());
+        for (final SiParser.BaseLevelContext t : ctx.types) {
+            list.add((Type) this.visit(t));
+        }
+
+        return list;
+    }
+
+    @Override
+    public Type visitCoreNomialType(SiParser.CoreNomialTypeContext ctx) {
+        switch (ctx.getText()) {
+            case "int":     return IntegerType.INT32;
+            case "double":  return ImmDouble.TYPE;
+            case "bool":    return ImmBoolean.TYPE;
+            case "byte":    return IntegerType.INT8;
+            case "char":    return ImmCharacter.TYPE;
+            case "string":  return ImmString.TYPE;
+            default:        throw new UnboundDefinitionException("Unknown primitive type: " + ctx.getText());
+        }
+    }
+
+    @Override
+    public Type visitUserDefType(SiParser.UserDefTypeContext ctx) {
+        final String rawName = ctx.base.getText();
+        String selectedName = null;
+        TypeBank<Type, Boolean> bank = null;
+
+        if (!rawName.contains("\\")) {
+            // It might be a type parameter (from generic types)
+            selectedName = rawName;
+            bank = this.definedTypes.get(rawName);
+        }
+
+        if (bank == null) {
+            // It might be a file-level type
+            selectedName = this.visitNamespacePath(ctx.base);
+            bank = this.definedTypes.get(selectedName);
+        }
+
+        if (bank == null) {
+            throw new UnboundDefinitionException("Attempt to use undefined type: " + rawName);
+        }
+
+        // This has to be a simple type (based on grammar)
+        if (!bank.hasSimpleType()) {
+            if (bank.hasParametricType()) {
+                throw new TypeMismatchException("Missing type paramters for type: " + selectedName);
+            }
+            throw new UnboundDefinitionException("Unbound definition for type: " + selectedName);
+        }
+
+        if (!bank.getSimpleMapping()) {
+            // If hidden, check if namespace allows us to do so
+            if (!isAccessible(selectedName, this.namespacePrefix)) {
+                throw new UnboundDefinitionException("Using hidden type: " + selectedName + " from: " + this.namespacePrefix);
+            }
+        }
+        return bank.getSimpleType();
+    }
+
+    @Override
+    public Type visitParametrizeGeneric(SiParser.ParametrizeGenericContext ctx) {
+        final String name = this.visitNamespacePath(ctx.base);
+        final TypeBank<Type, Boolean> bank = this.definedTypes.get(name);
+        if (bank == null) {
+            throw new UnboundDefinitionException("Attempt to use undefined type: " + name);
+        }
+
+        final List<Type> args = this.visitTypeParams(ctx.args);
+        try {
+            final ParametricType<Type> pt = bank.selectParametrization(args);
+            if (!bank.getParametricMapping(pt)) {
+                // If hidden, check if namespace allows us to do so
+                if (!isAccessible(name, this.namespacePrefix)) {
+                    throw new UnboundDefinitionException("Using hidden type: " + name + " from: " + this.namespacePrefix);
+                }
+            }
+            return pt.parametrize(args);
+        } catch (TypeMismatchException ex) {
+            throw new TypeMismatchException("Cannot parametrize type: " + name, ex);
+        }
+    }
+
+    @Override
+    public Type visitTypeParenthesis(SiParser.TypeParenthesisContext ctx) {
+        final Type input = ctx.e == null ? UnitType.INSTANCE : this.visitCoreTypes(ctx.e);
+        if (ctx.out == null) {
+            // the parenthesis was just to group types
+            return input;
+        }
+        return new FunctionType(input, (Type) this.visit(ctx.out));
+    }
+
+    @Override
+    public Type visitTupleLevel(SiParser.TupleLevelContext ctx) {
+        final ArrayList<Type> list = new ArrayList<>(ctx.t.size());
+        for (final SiParser.BaseLevelContext t : ctx.t) {
+            list.add((Type) this.visit(t));
+        }
+
+        if (list.size() == 1) {
+            return list.get(0);
+        }
+        return new TupleType(list);
+    }
+
+    @Override
+    public Type visitExtensionLevel(SiParser.ExtensionLevelContext ctx) {
+        final ArrayList<Type> list = new ArrayList<>(ctx.t.size());
+        for (final SiParser.TupleLevelContext t : ctx.t) {
+            list.add(this.visitTupleLevel(t));
+        }
+
+        if (list.size() != 1) {
+            throw new UnsupportedOperationException("Extension type is not supported (yet)");
+        }
+        return list.get(0);
+    }
+
+    @Override
+    public Type visitCoreTypes(SiParser.CoreTypesContext ctx) {
+        final ArrayList<Type> list = new ArrayList<>(ctx.t.size());
+        for (final SiParser.ExtensionLevelContext t : ctx.t) {
+            list.add(this.visitExtensionLevel(t));
+        }
+
+        if (list.size() != 1) {
+            throw new UnsupportedOperationException("Variant type is not supported (yet)");
+        }
+        return list.get(0);
+    }
+
+    @Override
     public Object visitDeclNativeFunc(SiParser.DeclNativeFuncContext ctx) {
-        System.out.println("scope =   " + this.namespacePrefix);
-        System.out.println("  native: " + ctx.name.getText());
+        System.out.println("scope  =   " + this.namespacePrefix);
+        System.out.println("native : " + ctx.name.getText());
         return null;
     }
 
     @Override
     public Object visitDeclFunc(SiParser.DeclFuncContext ctx) {
-        System.out.println("scope =   " + this.namespacePrefix);
-        System.out.println("  func:   " + ctx.name.getText());
+        System.out.println("scope  =   " + this.namespacePrefix);
+        System.out.println(" func  :   " + ctx.name.getText());
         return null;
+    }
+
+    private static boolean isAccessible(String id, String accScope) {
+        // id = \com\ymcmp\si\lang\id
+        // is accessible if accScope starts with \com\ymcmp\si\lang\
+        return (accScope + '\\').startsWith(id.substring(0, id.lastIndexOf('\\') + 1));
     }
 }
